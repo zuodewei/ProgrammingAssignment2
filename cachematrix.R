b34563a7aa9ba08674c80e2e5a5a48d1c53c5ff8
左德伟 <zuodewei@zuodeweideMacBook-Pro.local>
2025-03-03 21:46:24 +0800

    Completed Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL  # 用于缓存逆矩阵
 
+    set <- function(y) {
+        x <<- y    # 重新设置矩阵
+        inv <<- NULL  # 重置 inverse 缓存
+    }
+    get <- function() x
+
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+
+    list(set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)  # 这里假定矩阵可逆
+    x$setinverse(inv)
+    inv
 }
+
